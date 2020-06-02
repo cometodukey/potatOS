754aefddc83cecf992522f4574207f8cc1c8a2ce
Edward Bruce <ecbruce23@gmail.com>
2020-06-02 14:27:23 +0100

    fix ACPI table enumeration

--- include/kernel/acpi/acpi.h
+++ include/kernel/acpi/acpi.h
@@ -2,37 +2,9 @@
 #define __ACPI_H__
 
 #include <kernel/types.h>
-
-typedef struct {
-    char signature[8];
-    uint8_t checksum;
-    char oem_id[6];
-    uint8_t rev;
-    uint32_t rsdt_addr;
-    /* v2+ only */
-    uint32_t length;
-    uint64_t xsdt_addr;
-    uint8_t ext_checksum;
-    uint8_t reserved[3];
-} __attribute__((packed)) Rsdp;
-
-typedef struct {
-    char signature[4];
-    uint32_t length;
-    uint8_t rev;
-    uint8_t checksum;
-    char oem_id[6];
-    char oem_table_id[8];
-    uint32_t oem_rev;
-    uint32_t creator_id;
-    uint32_t creator_rev;
-} __attribute__((packed)) Sdt;
-
-typedef struct {
-    Sdt sdt;
-    uint32_t *sdt_ptr;
-} __attribute__((packed)) Rsdt;
+#include <kernel/acpi/tables.h>
 
 void init_acpi(void);
+void init_acpi_fadt(Fadt *fadt);
 
 #endif /* __ACPI_H__ */
--- kernel/acpi/acpi.c
+++ kernel/acpi/acpi.c
@@ -1,4 +1,5 @@
 #include <kernel/acpi/acpi.h>
+#include <kernel/acpi/tables.h>
 #include <kernel/types.h>
 #include <kernel/lib/mem.h>
 #include <kernel/lib/panic.h>
@@ -14,36 +15,26 @@ Rsdt *rsdt;
 void
 init_acpi(void) {
     uint16_t ebda;
-    size_t checksum;
+    uint8_t checksum;
 
     kputs("Initialising ACPI");
     if (find_rsdp_signature(0xE0000, 0xFFFFF, (void **)&rsdp) == GENERIC_ERR) {
+        kputs(" Did not find RSDP in 0xE0000 to 0xFFFFF");
         ebda = *(volatile uint16_t *)0x40e;
         if (find_rsdp_signature(ebda, ebda + 4096, (void **)&rsdp) == GENERIC_ERR) {
+            kputs(" Did not find RSDP in first 4KB of EBDA");
             PANIC("ACPI not supported");
         }
     }
-    kprintf(" Found RSDP at %p\r\n", rsdp);
+    kprintf(" Found Root System Description Pointer at %p\r\n", rsdp);
 
     checksum = memsum(rsdp, 20);
-    if ((uint8_t)checksum != 0) {
+    if (checksum != 0) {
         PANIC("ACPI checksum is invalid!");
     }
 
-    // TODO: support ACPI v2+
-    kputs(" RSDP info:");
-    kprintf("  Signature:    %.8s\r\n", rsdp->signature);
-    kprintf("  Checksum:     %x\r\n",   rsdp->checksum);
-    kprintf("  OEM ID:       %.6s\r\n", rsdp->oem_id);
-    kprintf("  Revision:     %u\r\n",   rsdp->rev);
-    kprintf("  RSDT address: %p\r\n",   rsdp->rsdt_addr);
-    if (rsdp->rev >= 2) {
-        /* v2+ */
-        // TODO: validate ext_checksum
-        kprintf(" Length:            %u\r\n", rsdp->length);
-        kprintf(" XSDT address:      %p\r\n", rsdp->xsdt_addr);
-        kprintf(" Extended checksum: %x\r\n", rsdp->ext_checksum);
-    }
+    kprintf("  OEM ID:   %.6s\r\n", rsdp->oem_id);
+    kprintf("  Revision: %u\r\n", rsdp->rev);
 
     rsdt = (Rsdt *)rsdp->rsdt_addr;
 
@@ -64,19 +55,22 @@ find_rsdp_signature(uintptr_t start, uintptr_t end, void **found) {
 
 static void
 init_acpi_tables(void) {
-    uint8_t checksum = memsum((void *)&rsdt->sdt, rsdt->sdt.length);
+    size_t i = 0, entries;
+    Sdt *header;
+    uint8_t checksum = memsum((void *)&rsdt->root_sdt, rsdt->root_sdt.length);
     if (checksum != 0) {
         PANIC("Invalid SDT checksum!");
     }
+    entries = (rsdt->root_sdt.length - sizeof(Sdt)) / 4;
 
-    kputs(" SDT info:");
-    kprintf("  Signature:        %.4s\r\n", rsdt->sdt.signature);
-    kprintf("  Length:           %u\r\n",   rsdt->sdt.length);
-    kprintf("  Revision:         %u\r\n",   rsdt->sdt.rev);
-    kprintf("  Checksum:         %x\r\n",   rsdt->sdt.checksum);
-    kprintf("  OEM ID:           %.6s\r\n", rsdt->sdt.oem_id);
-    kprintf("  OEM Table ID:     %.8s\r\n", rsdt->sdt.oem_table_id);
-    kprintf("  OEM Revision:     %u\r\n",   rsdt->sdt.oem_rev);
-    kprintf("  Creator ID:       %u\r\n",   rsdt->sdt.creator_id);
-    kprintf("  Creator Revision: %u\r\n",   rsdt->sdt.creator_rev);
+    kprintf(" Found Root System Description Table at %p\r\n", rsdt->root_sdt);
+    for (i = 0; i < entries; ++i) {
+        header = (Sdt *)(rsdt->sdt_ptr[i]);
+        if (!memcmp(header, "FACP", 4)) {
+            kprintf(" Found Fixed ACPI Description Table at %p\r\n", header);
+            init_acpi_fadt((Fadt *)header);
+        } else {
+            kprintf(" Found SDT %.4s at %p\r\n", header, header);
+        }
+    }
 }
