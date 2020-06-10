be8ea0d16e60393de828d65f7ba5fee74a248e77
Edward Bruce <ecbruce23@gmail.com>
2020-06-10 12:34:40 +0100

    fix stack smashing false positive with -Os optimisation

--- kernel/arch/i686/cpu.c
+++ kernel/arch/i686/cpu.c
@@ -14,7 +14,7 @@ cpuid(uint32_t leaf, uint32_t subleaf, uint32_t *eax,
     uint32_t cpuid_max;
     __asm__ volatile ("cpuid;"
                       : "=a" (cpuid_max)
-                      : "a" (leaf & 0x80000000) : "rbx", "rcx", "rdx");
+                      : "a" (leaf & 0x80000000) : "ebx", "ecx", "edx");
     if (leaf > cpuid_max)
         return 0;
     __asm__ volatile ("cpuid;"
